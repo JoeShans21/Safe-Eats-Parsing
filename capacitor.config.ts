b3c3eb4be9634fcc71164e575e11c285a2612237
Jiaying Li <jli1010@ad.unc.edu>
2025-02-11 16:23:45 -0500

    Attempt to add capacitor to the app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.safeeats.app',
+  appName: 'SafeEats Restaurant',
+  webDir: 'build'
+};
+
+export default config;
