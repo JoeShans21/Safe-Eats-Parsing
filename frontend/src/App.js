040313ebc45d2d9b09a4d1e12ede72766db00e23
Jiaying Li <jli1010@ad.unc.edu>
2025-02-28 13:42:04 -0500

    Styled navbar and re-added routing functionality to the all restaurants page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,30 +10,24 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
-        <nav className="bg-white shadow-sm">
-          <div className="max-w-6xl mx-auto px-4">
-            <div className="flex justify-between h-16">
-              <div className="flex">
-                <div className="flex-shrink-0 flex items-center">
-                  {/* <span className="text-xl font-bold text-blue-600">SafeEats</span> */}
-                  <Link to="/" className="text-xl font-bold text-blue-600">SafeEats</Link>
-                </div>
-                <div className="ml-6 flex space-x-4 items-center">
-                  <Link to="/" className="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md">
-                    Home
-                  </Link>
-                  <Link to="/add-restaurant" className="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md">
-                    Add Restaurant
-                  </Link>
-                </div>
-              </div>
-            </div>
+        <nav className="bg-white shadow-sm font-[Roboto_Flex] flex ml-5">
+          <div className="flex-shrink-0 flex items-center">
+            <Link to="/" className="text-3xl font-bold text-[#8DB670] m-4">SafeEats</Link>
+          </div>
+          <div className="ml-6 flex space-x-4 items-center">
+            <Link to="/restaurant-list" className="text-gray-700 hover:text-[#6c8b55] px-3 py-2 rounded-md">
+              Home
+            </Link>
+            <Link to="/add-restaurant" className="text-gray-700 hover:text-[#6c8b55] px-3 py-2 rounded-md">
+              Add Restaurant
+            </Link>
           </div>
         </nav>
         
         <main className="py-6">
           <Routes>
             <Route path="/" element={<LandingPage />} />
+            <Route path="/restaurant-list" element={<RestaurantList />} />
             <Route path="/add-restaurant" element={<AddRestaurant />} />
             <Route path="/restaurant/:restaurantId" element={<RestaurantPage />} />
             <Route path="/menu/:restaurantId" element={<AddMenuItem />} />
